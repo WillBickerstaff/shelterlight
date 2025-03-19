3194c7b2b2644a43fc883f4d0d1ded659b5ec7c6
Will Bickerstaff <will.bickerstaff@gmail.com>
2025-03-19 21:26:58 +0000

    Scheduler outline

---
+++ scheduler/Schedule.py
@@ -0,0 +1,23 @@
+"""scheduler.schedule.
+
+Copyright (c) 2025 Will Bickerstaff
+Licensed under the MIT License.
+See LICENSE file in the root directory of this project.
+
+Description: Generate daily schedules
+Author: Will Bickerstaff
+Version: 0.1
+"""
+
+class ScheduleGenerator:
+    def __init__(self):
+        pass
+
+    def daily_learning_cycle(self):
+        pass
+
+    def _evaluate_previous_day(self):
+       pass
+
+    def generate_schedule(self):
+        pass
